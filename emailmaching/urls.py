b78d284c2d0a3ca8884d4025d7470df4e0daafb8
salrealTeknet <salihuabduljawwad.a@gmail.com>
2024-05-01 21:28:33 +0100

    corrections

--- emailmaching/urls.py
+++ emailmaching/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import send_email_view
+from emailsender.views import send_email_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
